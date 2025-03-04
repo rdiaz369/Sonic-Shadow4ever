7de06a00e319d15db1187b7de6035a9b1d3df0fc
red849 <red849777@gmail.com>
2025-03-03 21:59:31 -0500

    Initial Commit

---
+++ react-vite/src/enviroments.js
@@ -0,0 +1,6 @@
+export default function() {
+    return {
+    APPLICATION_ID = "x9VHmPclyWUGBSfJu3yKWpz8O6XEVOQUaFGPspPH",
+    JAVASCRIPT_KEY: "mh8k1ktEzjCnRXY4rLnQBi2ojtax4yv35y93Z5gY",
+    SERVER_URL: "httpls://parseapi.back4app.com"
+}}
